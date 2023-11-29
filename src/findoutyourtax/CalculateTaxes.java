e0f1c67339c92aafdfecfec66bf8303aee9a4581
Felipe2022412 <2022412@student.cct.ie>
2023-11-29 14:02:34 +0000

    Deleting class TAX and adding to the calc class
    I had to delete the class taxes as i could not extend two classes to the calcTaxes class. That way, i put all the previous info in the cacl class. I am also creating the architecture of our methods

--- src/findoutyourtax/CalculateTaxes.java
+++ src/findoutyourtax/CalculateTaxes.java
@@ -5,11 +5,66 @@
 package findoutyourtax;
 
 /**
- *Class to calculate each tax and total amount owed
+ * Class to calculate each tax and total amount owed
+ *
  * @author felip
  */
-public class CalculateTaxes {
-    
-    
-    
+public class CalculateTaxes extends UserIncome {
+
+    private double paye;
+    private double usc;
+    private double prsi;
+    protected double totalTaxesOwen;
+
+    public double getPaye() {
+        return paye;
+    }
+
+    public void setPaye(double paye) {
+        this.paye = paye;
+    }
+
+    public double getUsc() {
+        return usc;
+    }
+
+    public void setUsc(double usc) {
+        this.usc = usc;
+    }
+
+    public double getPrsi() {
+        return prsi;
+    }
+
+    public void setPrsi(double prsi) {
+        this.prsi = prsi;
+    }
+
+    public double getTotalTaxesOwen() {
+        return totalTaxesOwen;
+    }
+
+    public void setTotalTaxesOwen(double totalTaxesOwen) {
+        this.totalTaxesOwen = totalTaxesOwen;
+    }
+
+    private double PAYECalc() {
+
+        return 0;
+
+    }
+
+    private double USCCalc() {
+
+        return 0;
+    }
+
+    private double PRSICalc() {
+
+        return 0;
+    }
+
+    private double incomeAfterCredit() {
+        return 0;
+    }
 }
