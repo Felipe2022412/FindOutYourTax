58e95fce493cee0a41ddc72ba1769c9e12b06321
Felipe2022412 <2022412@student.cct.ie>
2023-11-29 20:29:18 +0000

    Creating methods
    Created the methods to calculate the PAYE and USC. i HAD TO ALSO MODIFY THE TaxRange class to adeqaute to the method mentioned before.  WE STILL NEED TO REVIEW THE CALC!! will do more tomorrow

--- src/findoutyourtax/CalculateTaxes.java
+++ src/findoutyourtax/CalculateTaxes.java
@@ -29,11 +29,10 @@ public class CalculateTaxes extends User {
     public double getTaxCredits() {
         return taxCredits;
     }
-    
-     public double getPartnerGrossIncome() {
+
+    public double getPartnerGrossIncome() {
         return partnerGrossIncome;
     }
-
 
     public void setTaxCredits(double taxCredits) {
         this.taxCredits = taxCredits;
@@ -43,35 +42,58 @@ public class CalculateTaxes extends User {
         this.grossIncome = grossIncome;
     }
 
-   
     public void setPartnerGrossIncome(double partnerGrossIncome) {
         this.partnerGrossIncome = partnerGrossIncome;
     }
 
-   private double PAYECalc() {
-    if (!isMarried()) {
-        if (getGrossIncome() <= 40000) {
-            return Tax.PayeSingleLower.getRate() * getGrossIncome();
+    //Method to calculate the PAYE - the tax is progressive an over the gross amount
+    private double PAYECalc() {
+        if (!isMarried()) {
+            if (getGrossIncome() <= TaxRange.PayeSingleRange.getRange()) {
+                return Tax.PayeSingleLowerRate.getRate() * getGrossIncome();
+            } else {
+                return (TaxRange.PayeSingleRange.getRange() * Tax.PayeSingleLowerRate.getRate())
+                        + ((getGrossIncome() - TaxRange.PayeSingleRange.getRange()) * Tax.PayeSingleOverRate.getRate());
+
+            }
         } else {
-            return Tax.PayeSingleOver.getRate() * getGrossIncome();
-        }
-    } else {
-        if (getGrossIncome() > 40000) {
-            return Tax.PayeMarriedLower.getRate() * getGrossIncome();
-        } else {
-            return Tax.PayeMarriedOver.getRate() * getGrossIncome();
+            if (getGrossIncome() < TaxRange.PayeMarriedOneIncomeRange.getRange()) {
+                return Tax.PayeMarriedLowerRate.getRate() * getGrossIncome();
+            } else {
+                return (TaxRange.PayeMarriedOneIncomeRange.getRange() * Tax.PayeMarriedLowerRate.getRate())
+                        + ((getGrossIncome() - TaxRange.PayeMarriedTwoIncomesRange.getRange()) * Tax.PayeMarriedOverRate.getRate());
+            }
         }
     }
-}
 
+    //Method to calculate the USC - the tax is progressive an over the gross amount
     private double USCCalc() {
 
-        if () {
+        if (getGrossIncome() < TaxRange.USCRangeOne.getRange()) {
+            return Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange();
+        } else if (getGrossIncome() > TaxRange.USCRangeOne.getRange() && getGrossIncome() < TaxRange.USCRangeTwo.getRange()) {
+            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
+                    + ((getGrossIncome() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassTwoRate.getRate());
+        } else if (getGrossIncome() > TaxRange.USCRangeTwo.getRange() && getGrossIncome() < TaxRange.USCRangeThree.getRange()) {
+            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
+                    + ((TaxRange.USCRangeOne.getRange() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassTwoRate.getRate())
+                    + ((getGrossIncome() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassThreeRate.getRate());
         } else {
+            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
+                    + ((TaxRange.USCRangeOne.getRange() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassTwoRate.getRate())
+                    + ((TaxRange.USCRangeTwo.getRange() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassThreeRate.getRate())
+                    + ((getGrossIncome() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassFourRate.getRate());
+                    
         }
     }
 
-    private double PRSICalc() {
+
+
+//    // USCClassOneRate(0.005), //GROSS INCOME UNDER 12.012 EURO 0.5%
+//    USCClassTwoRate(0.02), //GROSS INCOME FROM 12,012.01 TO 22,920 EURO 2%
+//    USCClassThreeRate(0.045), //GROSS INCOME FROM 22,920.01 TO 70,044 EURO 4.5%
+//    USCClassFourRate(
+private double PRSICalc() {
 
         if () {
         } else {
