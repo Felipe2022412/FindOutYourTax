5801172281da9493a685729569ad50a7c48fa35a
Felipe2022412 <2022412@student.cct.ie>
2023-11-29 18:47:01 +0000

    Update tax enum class
    I just set up all the rates and ranges for the three taxes. They are on an annual basis €€€€€

--- src/findoutyourtax/Tax.java
+++ src/findoutyourtax/Tax.java
@@ -6,15 +6,25 @@ package findoutyourtax;
 
 /**
  *Enum for each tax in Ireland
+ * All gross incomes are calculated on annual basis
  * @author felip
  */
 public enum Tax {
     
      
-    INCOME_TAX(0.2),  // Example value: 20%
-    USC(0.05),        // Example value: 5%
-    PRSI(0.1);        // Example value: 10%
+    PayeSingleLower(0.2),//GROSS INCOME UNDER 40.000 EURO, 20%
+    PayeSingleOver(0.4),//GROSS INCOMEOVER 40.000 EURO, 40%
+    PayeMarriedLower(0.2),//GROSS INCOME UNDER 40.000 EURO, 20%
+    PayeMarriedOver(0.4),//GROSS INCOME OVER 40.000 EURO, 40%
 
+    USCClassOne(0.005), //GROSS INCOME UNDER 12.012 EURO 0.5%
+    USCClassTwo(0.02), //GROSS INCOME FROM 12,012.01 TO 22,920 EURO 2%
+    USCClassThree(0.045), //GROSS INCOME FROM 22,920.01 TO 70,044 EURO 4.5%
+    USCClassFour(0.08), //GROSS INCOME FROM 70,044.01 EURO 8%
+    
+    PRSIClassOne(0.0),  //GROSS INCOME UNDER 18,354.30 EURO
+    PRSIClassTwo(0.04); //GROSS INCOME OVER 18,354.30 EURO
+    
     private final double rate;
 
     Tax(double rate) {
