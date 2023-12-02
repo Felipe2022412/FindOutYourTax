e73a92b16676a24d62f11ed19d826caa12ea7b2a
Felipe2022412 <2022412@student.cct.ie>
2023-12-02 23:33:19 +0000

    Finished methods to calculate
    hey brah! just finished the methods to calculate each tax :)

--- src/findoutyourtax/CalculateTaxes.java
+++ src/findoutyourtax/CalculateTaxes.java
@@ -14,12 +14,88 @@ public class CalculateTaxes extends User {
 
     protected double grossIncome;
     protected double taxCredits;
+    protected double incomeAfterCredits = grossIncome - taxCredits;
     protected double partnerGrossIncome;
     protected double totalTaxesOwed;
     protected double liquidAmount;
 
     public CalculateTaxes(String firstName, String lastName, String userName, String password, String dateOfBirthday, String ppsNo, String email, boolean married) {
         super(firstName, lastName, userName, password, dateOfBirthday, ppsNo, email, married);
+    }
+   
+
+    //Method to calculate the PAYE - the tax is progressive an over the gross amount
+    private double PAYECalc() {
+        if (!isMarried()) {
+            if (getIncomeAfterCredits() <= TaxRange.PayeSingleRange.getRange()) {
+                return Tax.PayeSingleLowerRate.getRate() * getIncomeAfterCredits();
+            } else {
+                return (TaxRange.PayeSingleRange.getRange() * Tax.PayeSingleLowerRate.getRate())
+                        + ((getIncomeAfterCredits() - TaxRange.PayeSingleRange.getRange()) * Tax.PayeSingleOverRate.getRate());
+
+            }
+        } else {
+            if (getIncomeAfterCredits() <= TaxRange.PayeMarriedOneIncomeRange.getRange()) {
+                return Tax.PayeMarriedLowerRate.getRate() * getIncomeAfterCredits();
+            } else {
+                return (TaxRange.PayeMarriedOneIncomeRange.getRange() * Tax.PayeMarriedLowerRate.getRate())
+                        + ((getIncomeAfterCredits() - TaxRange.PayeMarriedOneIncomeRange.getRange()) * Tax.PayeMarriedOverRate.getRate());
+            }
+        }
+    }
+
+    //Method to calculate the USC - the tax is progressive an over the gross amount
+    private double USCCalc() {
+
+        if (getIncomeAfterCredits() <= TaxRange.USCRangeOne.getRange()) {
+            return Tax.USCClassOneRate.getRate() * getIncomeAfterCredits();
+        } else if (getIncomeAfterCredits() > TaxRange.USCRangeOne.getRange() && getIncomeAfterCredits() < TaxRange.USCRangeTwo.getRange()) {
+            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
+                    + ((getIncomeAfterCredits() - TaxRange.USCRangeOne.getRange()) * Tax.USCClassTwoRate.getRate());
+        } else if (getIncomeAfterCredits() > TaxRange.USCRangeTwo.getRange() && getIncomeAfterCredits() < TaxRange.USCRangeThree.getRange()) {
+            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
+                    + ((TaxRange.USCRangeTwo.getRange() - TaxRange.USCRangeOne.getRange()) * Tax.USCClassTwoRate.getRate())
+                    + ((getIncomeAfterCredits() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassThreeRate.getRate());
+        } else {
+            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
+                    + ((TaxRange.USCRangeTwo.getRange() - TaxRange.USCRangeOne.getRange()) * Tax.USCClassTwoRate.getRate())
+                    + ((TaxRange.USCRangeThree.getRange() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassThreeRate.getRate())
+                    + ((getIncomeAfterCredits() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassFourRate.getRate());
+
+        }
+    }
+
+//    // USCClassOneRate(0.005), //GROSS INCOME UNDER 12.012 EURO 0.5%
+//    USCClassTwoRate(0.02), //GROSS INCOME FROM 12,012.01 TO 22,920 EURO 2%
+//    USCClassThreeRate(0.045), //GROSS INCOME FROM 22,920.01 TO 70,044 EURO 4.5%
+//    USCClassFourRate(
+    private double PRSICalc() {
+
+        if (getIncomeAfterCredits() <= TaxRange.PRSIRange.getRange()) {
+            return getIncomeAfterCredits() * Tax.PRSIClassOneRate.getRate();
+        } else {
+            return getIncomeAfterCredits() * Tax.PRSIClassTwoRate.getRate();
+        }
+    }
+
+    private double liquidAmount() {
+        return getIncomeAfterCredits() - PAYECalc() - USCCalc() - PRSICalc();
+    }
+
+    private double totalTaxOwed() {
+        return PAYECalc() + USCCalc() + PRSICalc();
+    }
+    
+    public double getIncomeAfterCredits() {
+        return incomeAfterCredits;
+    }
+
+    public double getTotalTaxesOwed() {
+        return totalTaxesOwed;
+    }
+
+    public double getLiquidAmount() {
+        return liquidAmount;
     }
 
     public double getGrossIncome() {
@@ -45,68 +121,5 @@ public class CalculateTaxes extends User {
     public void setPartnerGrossIncome(double partnerGrossIncome) {
         this.partnerGrossIncome = partnerGrossIncome;
     }
-
-    //Method to calculate the PAYE - the tax is progressive an over the gross amount
-    private double PAYECalc() {
-        if (!isMarried()) {
-            if (getGrossIncome() <= TaxRange.PayeSingleRange.getRange()) {
-                return Tax.PayeSingleLowerRate.getRate() * getGrossIncome();
-            } else {
-                return (TaxRange.PayeSingleRange.getRange() * Tax.PayeSingleLowerRate.getRate())
-                        + ((getGrossIncome() - TaxRange.PayeSingleRange.getRange()) * Tax.PayeSingleOverRate.getRate());
-
-            }
-        } else {
-            if (getGrossIncome() < TaxRange.PayeMarriedOneIncomeRange.getRange()) {
-                return Tax.PayeMarriedLowerRate.getRate() * getGrossIncome();
-            } else {
-                return (TaxRange.PayeMarriedOneIncomeRange.getRange() * Tax.PayeMarriedLowerRate.getRate())
-                        + ((getGrossIncome() - TaxRange.PayeMarriedTwoIncomesRange.getRange()) * Tax.PayeMarriedOverRate.getRate());
-            }
-        }
-    }
-
-    //Method to calculate the USC - the tax is progressive an over the gross amount
-    private double USCCalc() {
-
-        if (getGrossIncome() < TaxRange.USCRangeOne.getRange()) {
-            return Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange();
-        } else if (getGrossIncome() > TaxRange.USCRangeOne.getRange() && getGrossIncome() < TaxRange.USCRangeTwo.getRange()) {
-            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
-                    + ((getGrossIncome() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassTwoRate.getRate());
-        } else if (getGrossIncome() > TaxRange.USCRangeTwo.getRange() && getGrossIncome() < TaxRange.USCRangeThree.getRange()) {
-            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
-                    + ((TaxRange.USCRangeOne.getRange() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassTwoRate.getRate())
-                    + ((getGrossIncome() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassThreeRate.getRate());
-        } else {
-            return (Tax.USCClassOneRate.getRate() * TaxRange.USCRangeOne.getRange())
-                    + ((TaxRange.USCRangeOne.getRange() - TaxRange.USCRangeTwo.getRange()) * Tax.USCClassTwoRate.getRate())
-                    + ((TaxRange.USCRangeTwo.getRange() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassThreeRate.getRate())
-                    + ((getGrossIncome() - TaxRange.USCRangeThree.getRange()) * Tax.USCClassFourRate.getRate());
-                    
-        }
-    }
-
-
-
-//    // USCClassOneRate(0.005), //GROSS INCOME UNDER 12.012 EURO 0.5%
-//    USCClassTwoRate(0.02), //GROSS INCOME FROM 12,012.01 TO 22,920 EURO 2%
-//    USCClassThreeRate(0.045), //GROSS INCOME FROM 22,920.01 TO 70,044 EURO 4.5%
-//    USCClassFourRate(
-/*private double PRSICalc() {
-
-        if () {
-        } else {
-        }
-    }
-
-    private double liquidAmount() {
-        if () {
-        } else {
-        }
-    }
-
-    private double totalTaxOwed() {
-
-    }*/
+    
 }
