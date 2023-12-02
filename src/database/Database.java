f53e59973ed3e3ed4beea955ad01f156698e06e3
Douglas Vinicius Dierings <2022322@student.cct.ie>
2023-12-02 17:40:35 +0000

    Create Database class
    Database created with the user logins, need to be add the tables name when the CHEN is created

---
+++ src/database/Database.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+/**
+ *
+ * @author dougl
+ */
+public abstract class Database {
+    
+    protected final static String DB_BASE_URL = "jdbc:mysql://localhost";
+    protected final static String USER = "ooc2023";
+    protected final static String PASSWORD = "ooc2023";
+    protected final static String DB_NAME = "";
+    //protected final static String TABLE_NAME = "patientData"; Alterar para as nossas tables
+    
+}
