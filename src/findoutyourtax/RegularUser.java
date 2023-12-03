7698af463ece7ac413f84af16eb1b0d93a1ce063
Felipe2022412 <2022412@student.cct.ie>
2023-12-03 12:00:02 +0000

    Update RegularUser.java
    Just added the methods to view and modify in the class

--- src/findoutyourtax/RegularUser.java
+++ src/findoutyourtax/RegularUser.java
@@ -8,10 +8,84 @@ package findoutyourtax;
  *
  * @author dougl
  */
-public class RegularUser extends User{
-    
+public class RegularUser extends User implements Modify, ViewRegularUser {
+
     public RegularUser(String firstName, String lastName, String userName, String password, String dateOfBirth, String ppsNo, String email, boolean married) {
         super(firstName, lastName, userName, password, dateOfBirth, ppsNo, email, married);
     }
-    
+
+    @Override
+    public String modifyFirstName(String newFirstName) {
+        setFirstName(newFirstName);
+        return getFirstName();
+    }
+
+    @Override
+    public String modifyLastName(String newLastName) {
+        setLastName(newLastName);
+        return getLastName();
+    }
+
+    @Override
+    public String modifyPassword(String newPassword) {
+        setPassword(newPassword);
+        return getPassword();
+    }
+
+    @Override
+    public String modifyDateOfBirth(String newDateOfBirth) {
+        setDateOfBirth(newDateOfBirth);
+        return getDateOfBirth();
+    }
+
+    @Override
+    public String modifyPPSNO(String newPPSNO) {
+        setPpsNo(newPPSNO);
+        return getPpsNo();
+    }
+
+    @Override
+    public String modifyEmail(String newEmail) {
+        setEmail(newEmail);
+        return getEmail();
+    }
+
+    @Override
+    public String viewUserNameRegularUser() {
+        return getUserName();
+
+    }
+
+    @Override
+    public String viewFirstNameRegularUser() {
+        return getFirstName();
+
+    }
+
+    @Override
+    public String viewLastNameRegularUser() {
+        return getLastName();
+    }
+
+    @Override
+    public String viewPasswordRegularUser() {
+        return getPassword();
+    }
+
+    @Override
+    public String viewDateOfBirthRegularUser() {
+        return getDateOfBirth();
+    }
+
+    @Override
+    public String viewPPSNORegularUser() {
+        return getPpsNo();
+    }
+
+    @Override
+    public String viewEmailRegularUser() {
+      return getEmail();
+    }
+
+  
 }
