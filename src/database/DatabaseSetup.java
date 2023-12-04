d2f19ca7652470dd80e0d4c145ef31070b43d7e0
Douglas Vinicius Dierings <146199335+Douglas2022322@users.noreply.github.com>
2023-12-04 16:23:36 +0000

    Update DatabaseSetup.java

--- src/database/DatabaseSetup.java
+++ src/database/DatabaseSetup.java
@@ -4,13 +4,9 @@
  */
 package database;
 
-import findoutyourtax.User;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
